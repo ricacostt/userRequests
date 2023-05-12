2a7d31295dbda4435bfa6951b57743e37202676c
Rich Griffiths <rich.griffiths89@gmail.com>
2023-05-12 11:46:11 +0100

    Startup project

--- new-user-requests/src/App.js
+++ new-user-requests/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="intro">
+      <img src='/pobl-logo.png' className="App-logo" alt="logo" />
+      <h2>New Starter Requests</h2>
+      <p>Here is the blank project for you to start from for this technical test. <br/> See <a href="https://github.com/PoblGroup-Dev/Junior-Web-Dev-Practical" target="_blank" rel="noreferrer">readme</a> for information. Good Luck!</p>
     </div>
   );
 }
